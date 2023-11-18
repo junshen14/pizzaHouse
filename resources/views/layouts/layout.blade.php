31ef24f72d241a5881e4648ac99907aeb2b71360
junshen14 <junshenyeoh@gmail.com>
2023-11-18 22:52:47 +0800

    First Laravel Project

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- <link href="/style/main.css" rel="stylesheet">-->
+        <!-- Styles -->
+        @vite(['resources/sass/app.scss','resources/sass/main.scss'])
+    </head>
+    <body>
+
+      @yield('content')
+
+      <footer>
+        <p>Copyright 2020 Pizza House</p>
+      </footer>
+    </body>
+</html>
